acc86b176450ec28ce6e585e67ab7e98f571dd8d
diego <guatemaladiegojose@gmail.com>
2023-07-16 17:22:06 -0400

    deleted all comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,11 @@
-require 'pry'
-
-
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  ## Running the server requires  " bundle exec rake server"
-  ##Visiti Sinatra with Active Record: Get Request for more information.
-
-  # Add your routes here
-
-  #Bakeries
+ 
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json(include: :breads)
   end
 
-
-  ##posting for bread
   post '/bakeries/:bakery_id/breads' do
     new_bread = Bread.create({
       name: params[:name],
@@ -25,7 +15,6 @@ class ApplicationController < Sinatra::Base
     })
     new_bread.to_json
   end
-  ##posting for bakeries
 
   post '/bakeries' do
     new_bakery = Bakery.create({
@@ -35,7 +24,6 @@ class ApplicationController < Sinatra::Base
     new_bakery.to_json(include: :breads)
   end
 
-  ## Delete Method for bakeries
   delete '/bakeries/:id' do
     
     bakery = Bakery.find(params[:id])
@@ -44,7 +32,6 @@ class ApplicationController < Sinatra::Base
     
   end
 
-  ##Patch Method For Bakeries
   patch '/bakeries/:id' do
     bakery = Bakery.find(params[:id])
     bakery.update(
